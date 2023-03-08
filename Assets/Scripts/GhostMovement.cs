8bd3409179d251ae8768748dfd3f5a5d28e2ee76
efraitor_ <efraitor.contact@gmail.com>
2023-03-08 16:57:35 +0100

    Changes
    -Added Blinky movement

---
+++ Assets/Scripts/GhostMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostMovement : MonoBehaviour
+{
+    //Velocidad inicial del fantasma
+    public float speed = 4f;
+    //Necesitamos un array de posiciones llamado Waypoints(puntos de ruta). Cada fantasma puede tener un número de puntos de ruta distintos
+    public Transform[] waypoints;
+    //Inicializo la posición en la que se encuentra el fantasma en la posición 0 del array. Luego este valor irá variando
+    int currentWaypoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Distancia al punto de ruta, entre la posición actual y el punto de ruta hacia el que se está dirigiendo
+        float distanceToWaypoint = Vector2.Distance((Vector2)transform.position, (Vector2)waypoints[currentWaypoint].position);
+
+        //Si la distancia hasta el punto de ruta es menor que 0.1 es que he llegado a la posición
+        if(distanceToWaypoint < 0.1f)
+        {
+            //Si el número del punto en el que está el fantasma es menor que los que hay guardados
+            if(currentWaypoint < waypoints.Length - 1)
+            {
+                //Avanzamos al siguiente punto
+                currentWaypoint++;
+            }
+            //Si por el contrario el número del punto en el que está el fantasma es igual o mayor que los que hay guardados
+            else
+            {
+                //Reseteamos al primer punto de los guardados
+                currentWaypoint = 0;
+            }
+            
+        }
+        //Y si no ha llegado al destino el fantasma
+        else
+        {
+            //Creo un vector para moverme desde donde está el fanstama ahora mismo, hasta el siguiente Waypoint a una velocidad dada
+            Vector2 newPos = Vector2.MoveTowards(transform.position, waypoints[currentWaypoint].position, speed * Time.deltaTime);
+            //Hacemos que se mueva a la posición que le toca
+            GetComponent<Rigidbody2D>().MovePosition(newPos);
+        }
+    }
+}
